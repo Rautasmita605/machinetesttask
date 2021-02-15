b29ce8ff5e94d67c5c4c76d1aad803043c5b7ebe
unknown <rautasmita06@gmail.com>
2021-02-15 10:46:43 +0530

    First Commit

---
+++ db_test.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_test
+-- ------------------------------------------------------
+-- Server version	5.7.22-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_category`
+--
+
+DROP TABLE IF EXISTS `tbl_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_category` (
+  `n_cat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `s_category` varchar(45) DEFAULT NULL,
+  `s_category_description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`n_cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+LOCK TABLES `tbl_category` WRITE;
+/*!40000 ALTER TABLE `tbl_category` DISABLE KEYS */;
+INSERT INTO `tbl_category` VALUES (8,'cosmetic','cosmetic category product available');
+/*!40000 ALTER TABLE `tbl_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_product`
+--
+
+DROP TABLE IF EXISTS `tbl_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_product` (
+  `n_product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `s_produt_name` varchar(45) DEFAULT NULL,
+  `n_cat_id` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`n_product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+LOCK TABLES `tbl_product` WRITE;
+/*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
+INSERT INTO `tbl_product` VALUES (18,'cosmetic Powder','8');
+/*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'db_test'
+--
+
+--
+-- Dumping routines for database 'db_test'
+--
+/*!50003 DROP FUNCTION IF EXISTS `get_category_name` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_category_name`(id int(11)) RETURNS varchar(150) CHARSET latin1
+BEGIN
+declare category_name varchar(150);
+    select s_category into category_name from tbl_category where n_cat_id=id;
+    
+RETURN category_name;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-08 10:09:33
